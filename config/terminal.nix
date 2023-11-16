31320b095ed441d6dd8084910a2c3723dbde1f6b
Tom Albrighton <thomas.albrighton@four.io>
2023-11-16 10:30:39 +1030

    force lazygit to start in insert mode

--- config/terminal.nix
+++ config/terminal.nix
@@ -3,7 +3,16 @@
 
   extraConfigLua = ''
     local Terminal  = require('toggleterm.terminal').Terminal
-    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+    local lazygit = Terminal:new({
+      cmd = "lazygit",
+      hidden = true,
+      direction = "float",
+
+      on_open = function(term)
+        vim.cmd("startinsert!")
+        vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+      end,
+    })
 
     function _lazygit_toggle()
       lazygit:toggle()
