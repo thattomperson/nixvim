5962acad8794e745f1a761369a14ff395fbbea59
Tom Albrighton <thomas.albrighton@four.io>
2023-11-28 15:06:47 +1030

    Include indent-blankline

--- config/stuff.nix
+++ config/stuff.nix
@@ -2,6 +2,21 @@
   plugins.comment-nvim.enable = true;
   plugins.gitsigns.enable = true;
   plugins.gitblame.enable = true;
+  plugins.indent-blankline = {
+    enable = true;
+    exclude.filetypes = [
+      "lspinfo"
+      "packer"
+      "checkhealth"
+      "help"
+      "man"
+      "gitcommit"
+      "TelescopePrompt"
+      "TelescopeResults"
+      "''"
+      "startify"
+    ];
+  };
   plugins.neo-tree = {
     enable = true;
     window.position = "float";
