88db0af678092e7197a31dcea53cb2850fd27e1c
Thomas Albrighton <thomas.albrighton@four.io>
2024-04-05 14:43:22 +0000

    Reenable cmp sources

--- config/completion.nix
+++ config/completion.nix
@@ -2,19 +2,21 @@
   # Completion
   plugins.cmp = {
     enable = true;
+    autoEnableSources = true;
     settings = {
       #    snippet.expand = "luasnip";
       completion = { completeopt = "menu,menuone"; };
-      #    sources = [
-      #      { name = "nvim_lsp"; }
-      #      { name = "luasnip"; }
-      #      { name = "buffer"; }
-      #      { name = "nvim_lua"; }
-      #      { name = "path"; }
-      #    ];
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "buffer"; }
+        { name = "nvim_lua"; }
+        { name = "path"; }
+      ];
       #    preselect =
       #      "None"; # Don't preselect to avoid tabs completing things prematurely
       #    mappingPresets = [ "insert" ];
+      preselect = "cmp.PreselectMode.None";
       mapping = {
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<C-Space>" = "cmp.mapping.complete()";
@@ -22,12 +24,12 @@
     };
   };
 
-  plugins.luasnip.enable = true;
-  plugins.cmp_luasnip.enable = true;
-  plugins.cmp-buffer.enable = true;
-  plugins.cmp-path.enable = true;
-  plugins.cmp-nvim-lua.enable = true;
-  plugins.cmp-nvim-lsp.enable = true;
-  plugins.cmp-nvim-lsp-document-symbol.enable = true;
-  plugins.cmp-nvim-lsp-signature-help.enable = true;
+  # plugins.luasnip.enable = true;
+  # plugins.cmp_luasnip.enable = true;
+  # plugins.cmp-buffer.enable = true;
+  # plugins.cmp-path.enable = true;
+  # plugins.cmp-nvim-lua.enable = true;
+  # plugins.cmp-nvim-lsp.enable = true;
+  # plugins.cmp-nvim-lsp-document-symbol.enable = true;
+  # plugins.cmp-nvim-lsp-signature-help.enable = true;
 }
